ea5f8ca4231bca340cab1ce86593af2c0bf0f8d1
Nathan Cortez <nath.co03@icloud.com>
2025-04-09 22:32:26 -0400

    created first api route to get songs

--- app.js
+++ app.js
@@ -1,26 +1,41 @@
 //setup.. this is similar to when we use default tags in HTML
 
 const express = require("express");
+//have to use cors in order to host front and back on the same device
+var cors = require("cors");
 // be an express server
 const app = express();
+app.use(cors());
 const router = express.Router();
 
 //start web server....app.listen(portnumber, function)
 
-app.listen(3000, function () {
-  console.log("Listening on port 3000");
-});
-
 // making an api using routes
 //Routes are used to handle browser request. The look like URLs. The diff is that when a browser request a route, it is dynamically handled by using a function
 
-//GET or a reg request when someone goes to http://localhost:3000/hello
-//when using a function in a route, we almost always have a parameter or handle a response and request
+router.get("/songs", function (req, res) {
+  const songs = [
+    {
+      title: "We Found Love",
+      artist: "Rihanna",
+      popularity: 10,
+      releaseDate: new Date(2011, 9, 22),
+      genre: ["electro house"],
+    },
+    {
+      title: "Happy",
+      artist: "Pharrell Williams",
+      popularity: 10,
+      releaseDate: new Date(2013, 11, 21),
+      genre: ["soul", "new soul"],
+    },
+  ];
 
-app.get("/hello", function (req, res) {
-  res.send("<h1>Hello Express</h1>");
+  res.json(songs);
 });
 
-app.get("/goodbye", function (req, res) {
-  res.send("<h1>Goodbye Express!</h1>");
-});
+//all request that usually use an api start with /api.... /api/songs
+
+app.use("/api", router);
+
+app.listen(3000);
